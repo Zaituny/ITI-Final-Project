c771d001c19fbde330947099efaf990e50a4b5b4
H-a-m-za <hamzamazen2002.hm@gmail.com>
2024-07-10 12:39:11 +0300

    Player_Input

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
+    public Rigidbody rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    float horizontal;
+    float Speed;
+    float jumpingPower;
+    bool isFacingRight;
+    [SerializeField] Animator animator;
+
     // Start is called before the first frame update
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
     void Start()
     {
         
@@ -13,6 +27,50 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector2(horizontal * Speed, rb.velocity.y);
+        if (!isFacingRight && horizontal > 0f)
+        {
+            Flip();
+        }
+        else if (isFacingRight && horizontal < 0f)
+        {
+            Flip();
+        }
+    }
+    void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+        if (context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+    private void Hit(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            animator.SetBool("Hit", true);
+        }
+        if(context.canceled)
+        {
+            animator.SetBool("Hit",false);
+        }
+    }
+    bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector2 localScale = transform.localScale;
+        transform.localScale = localScale;
+    }
+    void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
     }
 }
