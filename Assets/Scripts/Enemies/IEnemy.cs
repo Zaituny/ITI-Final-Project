0da72c40ed76217f1802a64a9aa107d4e840ed81
Zaituny <mohamedinv.580@gmail.com>
2024-07-10 12:35:13 +0300

    added more scripts

--- Assets/Scripts/Enemies/IEnemy.cs
+++ Assets/Scripts/Enemies/IEnemy.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IEnemy : MonoBehaviour
+public interface IEnemy
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public void Patrol();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void Attack();
+
+    public void FollowPlayer();
+
+    public void ReturnToPatrol();
 }
